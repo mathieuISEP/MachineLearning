43892006f469c014d8ac8dbed4eb27e5a5aff51c
Anthony Demogue <anthony.demogue@gmail.com>
2017-12-06 10:39:06 +0100

    changement de hotel star en numerique

--- projectCode.R
+++ projectCode.R
@@ -5,6 +5,6 @@ lasvegas$HotelQuality[lasvegas$Score<4]="Low"
 attach(lasvegas)
 summary(lasvegas)
 lasvegasquant = lasvegas[,c(2,3,4,5,15)]
-lasvegasquant.reg = lm(Score ~ Nr..hotel.reviews + Nr..reviews + Helpful.votes + Hotel.stars,data = lasvegasquant)
+lasvegasquant.reg = lm(Score ~ Nr..hotel.reviews + Nr..reviews + Helpful.votes + as.numeric(Hotel.stars),data = lasvegasquant)
 summary(lasvegasquant.reg)
 plot(lasvegasquant.reg)
